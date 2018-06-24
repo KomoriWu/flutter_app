5a8ccc6c2d4ff2298dd4b9f4012e08e2fe955bda
komoriwu <tmwuzhen@qq.com>
2018-06-24 16:33:27 +0800

    listview列表点击事件，路由跳转

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 //          child: new RandomWords(),
 //        ),
 //      ),
-
+        theme: new ThemeData(primaryColor: Colors.white),
       home: new RandomWords(),
     );
   }
@@ -39,6 +39,7 @@ class RandomWordsState extends State<RandomWords> {
 //    return new Text(wordPair.asPascalCase);
 //  }
   final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
 
   @override
@@ -47,6 +48,9 @@ class RandomWordsState extends State<RandomWords> {
     return new Scaffold(
       appBar: new AppBar(
         title: new Text('Startup Name Generator'),
+        actions: <Widget>[
+          new IconButton(icon: new Icon(Icons.list), onPressed: _pushSaved)
+        ],
       ),
       body: _buildSuggestions(),
     );
@@ -66,11 +70,51 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
     return new ListTile(
       title: new Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+      trailing: new Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(new MaterialPageRoute(builder: (context) {
+      final title = _saved.map((pair) {
+        return new ListTile(
+          title: new Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+      final divided = ListTile
+          .divideTiles(
+            context: context,
+            tiles: title,
+          )
+          .toList();
+      return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Save Suggestions'),
+        ),
+        body: new ListView(children: divided),
+      );
+    }));
   }
 }
